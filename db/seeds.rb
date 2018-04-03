e5ee31e995ccdbf420a20afce4e21013d30866da
Josh Pigford <josh@joshpigford.com>
2018-04-03 15:14:07 -0500

    All the genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,23 +22,6 @@ Studio.create!([
     {name: "Pet Sounds Studio", description: "", engineer_name: Faker::FunnyName.name, cost: 10000},
 ])
 
-200.times {
-Member.create!([
-  {
-    #name: "#{Faker::Name.first_name} #{Faker::Name.last_name}",
-    name: Faker::FunnyName.name,
-    gender: ['M','F'].sample,
-    birthdate: rand(15..75).years.ago,
-    trait_ego: rand(0..100),
-    trait_looks: rand(0..100),
-    trait_creativity: (1..100).map { |i| (101 - i).times.map { i } }.flatten.sample,
-    trait_fatigue: 0,
-    skill_primary: Skill.find(Skill.ids.shuffle.first),
-    skill_primary_level: (1..100).map { |i| (101 - i).times.map { i } }.flatten.sample
-  }
-])
-}
-
 # Skills
 Skill.delete_all
 Skill.create!([
@@ -59,6 +42,22 @@ Skill.create!([
   { name: "Vocals" },
   { name: "Wind" }
 ])
+
+500.times {
+Member.create!([
+  {
+    #name: "#{Faker::Name.first_name} #{Faker::Name.last_name}",
+    name: Faker::FunnyName.name,
+    birthdate: rand(15..75).years.ago,
+    trait_ego: rand(0..100),
+    trait_looks: rand(0..100),
+    trait_creativity: (1..100).map { |i| (101 - i).times.map { i } }.flatten.sample,
+    trait_fatigue: 0,
+    skill_primary: Skill.find(Skill.ids.shuffle.first),
+    skill_primary_level: (1..100).map { |i| (101 - i).times.map { i } }.flatten.sample
+  }
+])
+}
 
 # Genres
 Genre.delete_all
@@ -147,3 +146,123 @@ Skill.find_by_name('Wind').genre_skills.create(genre_id: genre.id)
 
 genre = Genre.create(name: "Orchestral", style: "Concert Pianist", min_members: 1, max_members: 2)
 Skill.find_by_name('Piano').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Pop", style: "Brit Pop", min_members: 3, max_members: 5)
+Skill.find_by_name('Acoustic Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Pop", style: "Euro Pop", min_members: 3, max_members: 5)
+Skill.find_by_name('Keyboards').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Dance').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Pop", style: "Boy Band", min_members: 3, max_members: 5)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Dance').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Pop", style: "Girl Band", min_members: 3, max_members: 5)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Dance').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Pop", style: "Latin", min_members: 3, max_members: 5)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Dance').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Percussion').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Rock", style: "Gothic", min_members: 4, max_members: 6)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Keyboards').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Lead Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Rock", style: "Indie", min_members: 4, max_members: 6)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Keyboards').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Acoustic Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Rock", style: "Metal", min_members: 4, max_members: 6)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Lead Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Rhythm Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Rock", style: "Punk", min_members: 3, max_members: 5)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Lead Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Rock", style: "Rock", min_members: 4, max_members: 6)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Lead Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Rock", style: "Ska", min_members: 4, max_members: 6)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Keyboards').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Horns').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "Blues", min_members: 4, max_members: 6)
+Skill.find_by_name('Acoustic Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Horns').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Wind').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "Funk", min_members: 5, max_members: 7)
+Skill.find_by_name('Acoustic Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Keyboards').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Wind').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "Hip Hop", min_members: 3, max_members: 5)
+Skill.find_by_name('DJ').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Rapping').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "Jazz", min_members: 4, max_members: 6)
+Skill.find_by_name('Acoustic Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Bass Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Horns').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Wind').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "Rap", min_members: 2, max_members: 4)
+Skill.find_by_name('Dance').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('DJ').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Rapping').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "Raggae", min_members: 3, max_members: 5)
+Skill.find_by_name('Acoustic Guitar').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Drummer').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Dance').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Rapping').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Soul", style: "RnB", min_members: 2, max_members: 4)
+Skill.find_by_name('Keyboards').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('DJ').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Vocalist", style: "Barbership Quartet", min_members: 4, max_members: 4)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
+
+genre = Genre.create(name: "Vocalist", style: "Solo", min_members: 1, max_members: 1)
+Skill.find_by_name('Vocals').genre_skills.create(genre_id: genre.id)
