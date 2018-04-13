df1e7b6425e77ad94060682f02fdc98d927732cd
Ryan Kulp <ryanckulp@gmail.com>
2018-04-13 18:06:50 -0400

    ability to delete songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,30 @@
 class SongsController < ApplicationController
+  before_action :authenticate_manager!
+  before_action :set_band, only: [:destroy]
+  before_action :set_song, only: [:destroy]
+
   def index
   end
+
+  def destroy
+    if @song.destroy
+      @band.happenings.create(what: "#{@song.name} was thrown in the trash!")
+      redirect_to band_path(@band), alert: "#{@song.name} was deleted."
+    else
+      redirect_to root_path, alert: 'Something went wrong.'
+    end
+  end
+
+  private
+
+  def set_band
+    @band = current_manager.bands.find(params[:band_id]) rescue nil
+    redirect_to root_path, alert: "You can't do that." if @band.nil?
+  end
+
+  def set_song
+    @song = @band.songs.find(params[:id]) rescue nil
+    redirect_to root_path, alert: "You can't do that." if @song.nil?
+  end
+
 end
