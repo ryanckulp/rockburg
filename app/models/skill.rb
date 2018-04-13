259aeec27d3076a8853b4c09c738f862cdd47b3e
Ben Sharpe <bsharpe@gmail.com>
2018-04-12 18:21:55 -0600

    add verbs to skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,11 +4,14 @@
 #
 #  id         :bigint(8)        not null, primary key
 #  name       :string
+#  verb       :string           default("play")
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class Skill < ApplicationRecord
+  ensure_by :id, :name
+
   has_many :genre_skills
   has_many :genres, through: :genre_skills
   has_many :member_skills
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_09_213912) do
+ActiveRecord::Schema.define(version: 2018_04_13_001352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 2018_04_09_213912) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "verb", default: "play"
   end
 
   create_table "song_recordings", force: :cascade do |t|
