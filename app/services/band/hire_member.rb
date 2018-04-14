4522cbbc5200bf0b5be97f64289718107dca6ae6
Ben Sharpe <bsharpe@gmail.com>
2018-04-14 07:33:31 -0500

    use HireMember service

---
+++ app/services/band/hire_member.rb
@@ -0,0 +1,24 @@
+class Band::HireMember < ApplicationService
+
+  expects do
+    required(:band).filled
+    required(:skill).filled
+    required(:member).filled
+  end
+
+  delegate :band, :skill, :member, to: :context
+
+  before do
+    context.band = Band.ensure!(band)
+    context.skill = Skill.ensure!(skill)
+    context.fail!(message: "Not a skill for this band") unless band.genre.skills.where(id: skill.id).exists?
+    context.member = Member.ensure!(member)
+  end
+
+  def call
+    member_band = MemberBand.create!(band: band, skill: skill, member: member, joined_band_at: Time.current)
+    # broadcast(:hire_member, band: band, member: member)
+    band.manager.spend_money(member.cost_generator, band: band)
+    band.happenings.create(what: "#{member.name} was hired to #{member.primary_skill.verb} #{member.primary_skill.name}!")
+  end
+end
