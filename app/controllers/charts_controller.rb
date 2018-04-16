a536a9ebbb68c858615c305f1517bd21f3a80076
Josh Pigford <josh@joshpigford.com>
2018-04-16 18:37:48 -0500

    Only include managers in the charts who've created bands

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,6 @@
 class ChartsController < ApplicationController
   def index
-    @managers = Manager.includes(:most_recent_financial).order("financials.balance desc").limit(5)
+    @managers = Manager.includes(:most_recent_financial).joins(:bands).order("financials.balance desc").select("COUNT(bands.id) as bands_count").group('managers.id, financials.id').limit(5)
     @bands = Band.order("buzz desc").limit(5)
     @releases = Recording.order("sales desc").where.not(release_at: nil).limit(5)
   end
@@ -10,7 +10,7 @@ class ChartsController < ApplicationController
   end
 
   def managers
-    @managers = Manager.includes(:most_recent_financial).order("financials.balance desc").limit(30)
+    @managers = Manager.includes(:most_recent_financial).joins(:bands).order("financials.balance desc").select("COUNT(bands.id) as bands_count").group('managers.id, financials.id').limit(30)
   end
 
   def releases
