bba1ac47b5a40fe5dd371987a561e907482b3802
Ben Sharpe <bsharpe@gmail.com>
2018-04-17 08:19:38 -0600

    fix params passing and service names for daily worker + add test

--- app/services/manager/daily_financials.rb
+++ app/services/manager/daily_financials.rb
@@ -6,15 +6,15 @@ class Manager::DailyFinancials < ApplicationService
   delegate :manager, to: :context
 
   before do
-    context.manager = Manager.ensure(manager["manager"])
+    context.manager = Manager.ensure!(manager)
   end
 
   def call
     manager.bands.each do |band|
       result = Band::DailyUpdate.call(band: band)
       if result.success?
-        manager.sub_money(result.daily_running_costs, band: band)
-        manager.add_money(result.earnings, band: band)
+        Band::SpendMoney.(band: band, amount: result.daily_running_costs)
+        Band::EarnMoney.(band: band, amount: result.earnings )
       else
         # Probably should tell someone that the band couldn't update it's data
       end
--- app/workers/daily_running_financial_worker.rb
+++ app/workers/daily_running_financial_worker.rb
@@ -1,7 +1,7 @@
 class DailyRunningFinancialWorker < ApplicationWorker
   def perform
     Manager.find_each do |manager|
-      DailyManagerWorker.perform_async(manager: manager.to_global_id)
+      DailyManagerWorker.perform_async(manager.to_global_id)
     end
   end
 end
